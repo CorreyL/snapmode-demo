88791a78937074e4dfa4d00e2e51ea1f85d81c38
Correy Lim <correylim@gmail.com>
2021-07-07 09:47:44 -0700

    Initial commit

---
+++ precache-manifest.bbe4b70c786815d5be1d18ec3d350679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "955e5345007228600018",
+    "url": "/snapmode-demo/static/css/main.6fbb8fc8.chunk.css"
+  },
+  {
+    "revision": "955e5345007228600018",
+    "url": "/snapmode-demo/static/js/main.955e5345.chunk.js"
+  },
+  {
+    "revision": "78d43f1a99c80cd94a28",
+    "url": "/snapmode-demo/static/js/1.78d43f1a.chunk.js"
+  },
+  {
+    "revision": "714bed7576a17aa15b2a",
+    "url": "/snapmode-demo/static/js/runtime~main.714bed75.js"
+  },
+  {
+    "revision": "306a598d8cd978b53c7fd55516139d38",
+    "url": "/snapmode-demo/index.html"
+  }
+];
